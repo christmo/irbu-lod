93a121131c2205b9c1e29d50f510f7b5ba1ffb52
christmo99 <christmo99@b889b7c5-e3d6-d508-aebd-e931b667f282>
2011-12-29 07:37:00 +0000

    Dibujado de rutas en el mapa de open

--- trunk/IRBU_AND/src/irbu/lod/objetos/Puntos.java
+++ trunk/IRBU_AND/src/irbu/lod/objetos/Puntos.java
@@ -3,16 +3,26 @@
  */
 package irbu.lod.objetos;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * @author DellXPS_L401X
  * 
  */
-public class Puntos {
+public class Puntos implements Parcelable
+{
 
 	private int idPunto;
 	private double lon;
 	private double lat;
-	
+
+	public Puntos(Parcel in) {
+		this.idPunto = in.readInt();
+		this.lon = in.readDouble();
+		this.lat = in.readDouble();
+	}
+
 	public Puntos(int idPunto, double lon, double lat) {
 		super();
 		this.idPunto = idPunto;
@@ -42,6 +52,27 @@ public class Puntos {
 
 	public void setLat(double lat) {
 		this.lat = lat;
+	}
+
+	public static final Parcelable.Creator<Puntos> CREATOR = new Parcelable.Creator<Puntos>() {
+		public Puntos createFromParcel(Parcel in) {
+			return new Puntos(in);
+		}
+
+		public Puntos[] newArray(int size) {
+			return new Puntos[size];
+		}
+	};
+
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeInt(idPunto);
+		dest.writeDouble(lon);
+		dest.writeDouble(lat);
 	}
 
 }
