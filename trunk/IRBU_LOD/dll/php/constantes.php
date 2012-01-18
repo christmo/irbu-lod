ac29e076b4e2200ed617100d7a2688525e8fdec2
christmo99 <christmo99@b889b7c5-e3d6-d508-aebd-e931b667f282>
2012-01-18 23:19:18 +0000

    cambios en path eliminar y guardar imagenes e iconos en la utpl

--- trunk/IRBU_LOD/dll/php/constantes.php
+++ trunk/IRBU_LOD/dll/php/constantes.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * TODO: Buscar la forma de no quemar la RUTA del directorio
+ * Obtiene el PATH absoluto del directorio donde almacenar las imagenes.
  */
-//$dir_img = 'c:\\Users\\DellXPS_L401X\\Dropbox\\-=Tesis=-\\IRBU_LOD\\img\\datap\\';
-$dir_img = 'c:\\Users\\Usuario\\Dropbox\\-=Tesis=-\\IRBU_LOD\\img\\datap\\';
-//$dir_img = 'C:\\wamp\\www\\IRBU_LOD\\img\\datap\\';
+$dir_img = realpath('../../../img/datap/');
 
 /**
  * Carpeta donde se almacenan imagenes de las paradas
