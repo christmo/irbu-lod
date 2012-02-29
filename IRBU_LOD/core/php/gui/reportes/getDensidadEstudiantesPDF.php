60994c43c2c70ec126d1fb0008865da5f63b69e5
christmo99 <christmo99@b889b7c5-e3d6-d508-aebd-e931b667f282>
2012-02-29 19:48:13 +0000

    reportes pdf modificados y listos

--- IRBU_LOD/core/php/gui/reportes/getDensidadEstudiantesPDF.php
+++ IRBU_LOD/core/php/gui/reportes/getDensidadEstudiantesPDF.php
@@ -10,28 +10,33 @@ class PDF extends FPDF {
     // Tabla coloreada
     function FancyTable($header, $data) {
         // Colores, ancho de línea y fuente en negrita
-        $this->SetFillColor(255, 0, 0);
+        $this->SetFillColor(255, 140, 0);
         $this->SetTextColor(255);
-        $this->SetDrawColor(128, 0, 0);
+        $this->SetDrawColor(0, 0, 0);
         $this->SetLineWidth(.3);
-        $this->SetFont('', 'B');
+        $this->SetFont('Helvetica', 'B', 12);
         // Cabecera
-        $w = array(30, 100, 40);
-        for ($i = 0; $i < count($header); $i++)
+        $w = array(25, 35, 100,50,50);
+        for ($i = 0; $i < count($header); $i++){
             $this->Cell($w[$i], 7, $header[$i], 1, 0, 'C', true);
+        }
         $this->Ln();
         // Restauración de colores y fuentes
         $this->SetFillColor(224, 235, 255);
         $this->SetTextColor(0);
-        $this->SetFont('');
+        $this->SetFont('Helvetica', '', 12);
         // Datos
         $fill = false;
+        $j=0;
         foreach ($data as $row) {
-            $this->Cell($w[0], 6, number_format($row[0]), 'LR', 0, 'C', $fill);
-            $this->Cell($w[1], 6, $row[1], 'LR', 0, 'L', $fill);
-            $this->Cell($w[2], 6, number_format($row[2]), 'LR', 0, 'C', $fill);
+            $this->Cell($w[0], 6, $row[0], 'LR', 0, 'C', $fill);
+            $this->Cell($w[1], 6, $row[1], 'LR', 0, 'C', $fill);
+            $this->Cell($w[2], 6, $row[2], 'LR', 0, 'L', $fill);
+            $this->Cell($w[3], 6, $row[3], 'LR', 0, 'R', $fill);
+            $this->Cell($w[4], 6, $row[4], 'LR', 0, 'R', $fill);
             $this->Ln();
             $fill = !$fill;
+            $j++;
         }
         // Línea de cierre
         $this->Cell(array_sum($w), 0, '', 'T');
@@ -39,10 +44,10 @@ class PDF extends FPDF {
 
     function Titulo($title) {
         // Arial bold 15
-        $this->SetFont('Helvetica', 'B', 15);
+        $this->SetFont('Helvetica', 'B', 18);
         // Calculamos ancho y posición del título.
         $w = $this->GetStringWidth($title) + 6;
-        $this->SetX((210 - $w) / 2);
+        $this->SetX((280 - $w) / 2);
         // Título
         $this->Cell($w, 9, $title, 1, 1, 'C', false);
         // Salto de línea
@@ -52,16 +57,18 @@ class PDF extends FPDF {
 }
 
 if ($periodo == 'Todos') {
-    $consultaSql = "SELECT p.direccion as parada, count(*) as estudiantes, dir_img
-                FROM paradas p, parada_estudiante pe
-                WHERE P.ID_PARADA = PE.ID_PARADA
-                GROUP BY p.id_parada";
+    $consultaSql = "SELECT CI_EST, DIR_EST, LAT_CASA, LON_CASA FROM viviendas
+                        WHERE FECHA_HORA in (
+                        SELECT MAX(FECHA_HORA)
+                        FROM viviendas
+                        GROUP BY CI_EST)";
 } else {
-    $consultaSql = "SELECT p.direccion as parada, count(*) as estudiantes, dir_img
-                FROM paradas p, parada_estudiante pe
-                WHERE P.ID_PARADA = PE.ID_PARADA
-                AND PE.PERIODO = '$periodo'
-                GROUP BY p.id_parada";
+    $consultaSql = "SELECT CI_EST, DIR_EST, LAT_CASA, LON_CASA FROM viviendas
+                        WHERE FECHA_HORA in (
+                        SELECT MAX(FECHA_HORA)
+                        FROM viviendas
+                        WHERE PERIODO='$periodo'
+                        GROUP BY CI_EST)";
 }
 
 consulta($consultaSql);
@@ -71,21 +78,23 @@ $data = array();
 for ($i = 0; $i < count($resulset); $i++) {
     $fila = $resulset[$i];
     $aux[] = ($i + 1);
-    $aux[] = utf8_decode($fila["parada"]);
-    $aux[] = $fila["estudiantes"];
+    $aux[] = $fila["CI_EST"];
+    $aux[] = utf8_decode($fila["DIR_EST"]);
+    $aux[] = $fila["LAT_CASA"];
+    $aux[] = $fila["LON_CASA"];
     $data[] = $aux;
     $aux = null;
 }
 
 
 $pdf = new PDF();
-$title = utf8_decode('Reporte de Número de Estudiantes por Parada');
+$title = utf8_decode('Reporte de Número de Estudiantes');
 
 // Títulos de las columnas
-$header = array('# Paradas', 'Paradas', '# Estudiantes');
-$pdf->SetFont('Helvetica', '', 14);
+$header = array('# Est', utf8_decode("Cédula"), utf8_decode("Dirección"),'Latitud','Longitud');
+$pdf->SetFont('Helvetica', '', 12);
 
-$pdf->SetTopMargin(30);
+$pdf->SetTopMargin(20);
 $pdf->SetLeftMargin(20);
 $pdf->SetRightMargin(20);
 $pdf->AddPage('L', 'A4');
